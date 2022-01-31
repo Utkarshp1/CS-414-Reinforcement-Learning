Refined epsilon-greedy for cases where epsilon is greater than 1, implemented average reward metric and fixed bug in UCB and UCB1-Normal algorithms.

Multi-Armed Bandits/src/Experiment.py
@@ -8,7 +8,7 @@ from algorithms import Greedy
 from MultiArmedBandit import MultiArmedBandit
 from reward_distributions import Bernoulli
 from utils import create_inverse_schedule
-from metrics import MeanCummulativeRegret
+from metrics import MeanCummulativeRegret, AverageReward
 
 class Experiment:
     '''
@@ -67,6 +67,8 @@ class Experiment:
                 (self.num_runs, self.num_arms))
             self.results[algo_name]["optimal_arm"] = np.empty(
                 (self.num_runs, 1))
+            self.results[algo_name]["rewards"] = np.empty(
+                (self.num_runs, self.total_time))
             self.results[algo_name]["metrics"] = {}
             
     def _init_directory(self):
@@ -100,18 +102,24 @@ class Experiment:
                 self.results[algo_name]["regrets"][i, :] = algo.regrets
                 self.results[algo_name]["counts"][i, :] = algo.counts
                 self.results[algo_name]["optimal_arm"][i, 0] = (
-                    multi_armed_bandit.optimal_arm) 
+                    multi_armed_bandit.optimal_arm)
+                self.results[algo_name]["rewards"][i, :] = algo.rewards
                 
         self._calculate_metrics()
                 
     def _calculate_metrics(self):
+        avg_reward = AverageReward()
         for algo_name in self.results:
             for metric in self.metrics:
                 self.results[algo_name]["metrics"][metric.name] = metric(
                     self.results[algo_name]["regrets"]).tolist()
+                    
+            self.results[algo_name]["metrics"][avg_reward.name] = (
+                avg_reward(self.results[algo_name]["rewards"]).tolist())
                 
     def generate_plots(self):
-        for metric in self.metrics:
+        avg_reward = AverageReward()
+        for metric in self.metrics + [avg_reward]:
             plt.figure()
             for algo_name in self.results:
                 plt.plot(self.results[algo_name]["metrics"][metric.name], 
@@ -122,6 +130,8 @@ class Experiment:
             plt.ylabel("Metric Value")
             # plt.savefig(os.path.join(self.exp_dir, metric + ".png"))
             plt.show()
+        
+        
             
     def generate_report(self):
         self.generate_plots()

Multi-Armed Bandits/src/algorithms.py
@@ -20,6 +20,7 @@ class Algorithm:
         self.multi_arm_bandit = multi_arm_bandit
         self.total_time = total_time
         self.regrets = np.empty((self.total_time, ))
+        self.rewards = np.empty((self.total_time, ))
         self.counts = [0]*self.multi_arm_bandit.num_arms
         
     def _init_params(self):
@@ -135,6 +136,7 @@ class Greedy(Algorithm):
         self.regrets[time] = self.multi_arm_bandit.calculate_regret(
             reward, arm_index)
         self.counts[arm_index] += 1
+        self.rewards[time] = reward
         
         self.act_val_esti[arm_index] = running_avg_update(
             self.act_val_esti[arm_index],
@@ -143,7 +145,7 @@ class Greedy(Algorithm):
         )
         
         if self.eps_schedule:
-            self.eps = self.eps_schedule(time+1)
+            self.eps = np.min(1, self.eps_schedule(time+1))
             # print(self.eps)
         
         
@@ -249,6 +251,7 @@ class SoftmaxPolicy(Algorithm):
         self.regrets[time] = self.multi_arm_bandit.calculate_regret(
             reward, arm_index)
         self.counts[arm_index] += 1
+        self.rewards[time] = reward
         
         self.act_val_esti[arm_index] = running_avg_update(
             self.act_val_esti[arm_index],
@@ -357,6 +360,7 @@ class UCB(Algorithm):
         self.regrets[time] = self.multi_arm_bandit.calculate_regret(
             reward, arm_index)
         self.counts[arm_index] += 1
+        self.rewards[time] = reward
         
         self.act_val_esti[arm_index] = running_avg_update(
             self.act_val_esti[arm_index],
@@ -365,7 +369,7 @@ class UCB(Algorithm):
         )
         
         if self.C_schedule:
-            self.temp = self.C_schedule(time+1)
+            self.C = self.C_schedule(time+1)
             
     def _pick_next_arm(self):
         '''
@@ -463,6 +467,7 @@ class ThompsonSampling(Algorithm):
         self.regrets[time] = self.multi_arm_bandit.calculate_regret(
             reward, arm_index)
         self.counts[arm_index] += 1
+        self.rewards[time] = reward
         
         self.posterior[arm_index].posterior_update(reward)
         
@@ -648,6 +653,7 @@ class UCB1_Normal(Algorithm):
             reward, arm_index)
         self.counts[arm_index] += 1
         self.square_reward[arm_index] += reward**2
+        self.rewards[time] = reward
         
         self.act_val_esti[arm_index] = running_avg_update(
             self.act_val_esti[arm_index],
@@ -656,7 +662,7 @@ class UCB1_Normal(Algorithm):
         )
         
         if self.C_schedule:
-            self.temp = self.C_schedule(time+1)
+            self.C = self.C_schedule(time+1)
             
         self.time += 1
             

Multi-Armed Bandits/src/reward_distributions.py
@@ -79,7 +79,7 @@ class Normal(RewardDistribution):
     '''
         This class implements the Normal reward distribution.
     '''
-    def __init__(self, mean=None, variance=None):
+    def __init__(self, mean=None, variance=0.1):
         '''
             Arguments:
             ---------
@@ -89,6 +89,7 @@ class Normal(RewardDistribution):
         '''
         if mean is None or variance is None:
             mean, variance = self._init_params(mean, variance)
+        print(variance)
             
         super().__init__(mean, variance)
         
@@ -129,13 +130,8 @@ class Normal(RewardDistribution):
             Refer: Finite-time Analysis of the Multiarmed Bandit 
             Problem, Auer, Cesa-Bianchi and Fischer, 2002.
         '''
-        print(square_reward)
-        print(counts)
-        print(avg_reward)
+
         time = np.sum(counts)
         part1 = (square_reward - counts*(avg_reward**2))/(counts - 1)
-        print(part1)
         part2 = np.log(time - 1)/counts
-        print(part2)
-        print(np.sqrt(16*part1*part2))
         return np.sqrt(16*part1*part2)